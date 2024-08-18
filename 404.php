a3a30dde2c893d889d76fc7ffcda978e08dfccd6
yuki-rose <154495079+yuki-rose@users.noreply.github.com>
2024-08-19 00:44:17 +0900

    修正

--- 404.php
+++ 404.php
@@ -8,12 +8,20 @@
         </picture>
         <div class="c-main__archive-ttlset">
             <figcaption class="c-main__archivettl">Not Found:</figcaption>
-                <?php   global $wp_query;
-                        $search_query = get_search_query(); ?>
-                <h3 class="c-main__archivettl-sub">ページが見つかりません。</h3>
-            
+            <?php   global $wp_query;
+                    $search_query = get_search_query(); ?>
+            <h3 class="c-main__archivettl-sub">ページが見つかりません。</h3>
         </div>
     </figure>
+
+    <div class="c-archive">
+        <h2 class="c-archive__ttl">ページが見つかりません。</h2>
+        <article class="c-archive__txt notfound">お探しのページは、削除されたか、名前が変更された可能性があります。<br>
+            直接アドレスを入力された場合は、アドレスが正しく入力されているかもう一度ご確認下さい。<br>
+            ブラウザの再読み込みを行ってもこのページが表示される場合は、
+            <a href="<?php echo esc_url(home_url('/')); ?>">トップページ</a>から目的のページをお探しください。
+        </article>
+    </div>
 
     </div><!--l-main_contents-->
 </main>
